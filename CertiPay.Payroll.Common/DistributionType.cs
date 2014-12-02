357081b14b03ce0f15429ea262d3aa83a4626eea
Matt Wagner <mattgwagner@gmail.com>
2014-12-02 14:31:32 -0500

    Add distribution type enum

---
+++ CertiPay.Payroll.Common/DistributionType.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections.Generic;
+
+namespace CertiPay.Payroll.Common
+{
+    /// <summary>
+    /// How the distribution is calculated from the net paycheck amount
+    /// </summary>
+    public enum DistributionType
+    {
+        /// <summary>
+        /// The remaining amount of the payroll posting is send to the distribution
+        /// </summary>
+        Balance = 1,
+
+        /// <summary>
+        /// A given flat amount of the remaining paycheck is sent to the distribution i.e. $400 to savings
+        /// </summary>
+        FlatAmount = 2,
+
+        /// <summary>
+        /// A percentage of the remaining paycheck is sent to the distribution i.e. 25% to savings
+        /// </summary>
+        Percentage = 3
+    }
+
+    public class DistributionTypes
+    {
+        public static IEnumerable<DistributionType> Values()
+        {
+            yield return DistributionType.Balance;
+            yield return DistributionType.FlatAmount;
+            yield return DistributionType.Percentage;
+        }
+    }
+}
