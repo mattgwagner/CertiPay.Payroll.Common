25ee6ecd854381d73bcf6a002f2e04dc48ef2ed2
Matt Wagner <mattgwagner@gmail.com>
2014-12-02 10:42:54 -0500

    Add enum for how to calculate deductions

---
+++ CertiPay.Payroll.Common/CalculationType.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections.Generic;
+
+namespace CertiPay.Payroll.Common
+{
+    /// <summary>
+    /// Identifies the method to calculate the result
+    /// </summary>
+    public enum CalculationType
+    {
+        /// <summary>
+        /// Deduction is taken as a percentage of the gross pay
+        /// </summary>
+        PercentOfGrossPay = 1,
+
+        /// <summary>
+        /// Deduction is taken as a percentage of the net pay
+        /// </summary>
+        PercentOfNetPay = 2,
+
+        /// <summary>
+        /// Deduction is taken as a flat, fixed amount
+        /// </summary>
+        FixedAmount = 3,
+
+        /// <summary>
+        /// Deduction is taken as a fixed amount per hour of work
+        /// </summary>
+        FixedHourlyAmount = 4
+    }
+
+    public static class CalculationTypes
+    {
+        public static IEnumerable<CalculationType> Values()
+        {
+            yield return CalculationType.PercentOfGrossPay;
+            yield return CalculationType.PercentOfNetPay;
+            yield return CalculationType.FixedAmount;
+            yield return CalculationType.FixedHourlyAmount;
+        }
+    }
+}
