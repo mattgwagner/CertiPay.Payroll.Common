957ac3742a3b571098e85ab32e4e84a57c6b0c1c
Matt Wagner <mattgwagner@gmail.com>
2014-12-04 14:59:12 -0500

    Add method for rounding decimals.

--- CertiPay.Payroll.Common/ExtensionMethods.cs
+++ CertiPay.Payroll.Common/ExtensionMethods.cs
@@ -7,6 +7,16 @@ namespace CertiPay.Payroll.Common
     public static class ExtensionMethods
     {
         /// <summary>
+        /// Round the decimal to the given number of decimal places using the given method of rounding.
+        /// By default, this is 2 decimal places to the nearest even for middle values
+        /// i.e. 1.455 -> 1.46
+        /// </summary>
+        public static Decimal Round(this Decimal val, int decimals = 2, MidpointRounding rounding = MidpointRounding.ToEven)
+        {
+            return Math.Round(val, decimals, rounding);
+        }
+
+        /// <summary>
         /// Returns the display name from the description attribute on the enumeration, if available.
         /// Otherwise returns the ToString() value.
         /// </summary>
