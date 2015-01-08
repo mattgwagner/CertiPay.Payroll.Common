675697a32ffd39b0682c80ea956a0a08451f7ae9
Matt Wagner <mattgwagner@gmail.com>
2015-01-08 11:35:24 -0500

    Tweak garnishment types to have differentiation in tax garnishments, order of application

--- CertiPay.Payroll.Common/GarnishmentType.cs
+++ CertiPay.Payroll.Common/GarnishmentType.cs
@@ -4,31 +4,51 @@ using System.ComponentModel;
 namespace CertiPay.Payroll.Common
 {
     /// <summary>
-    /// A configured garnishment, or involuntary, ordered deduction from an employee's pay for things such
-    /// as unpaid taxes, child support, or bankruptcy
+    /// Wage garnishment, the most common type of garnishment, is the process of deducting money from an employee's monetary compensation (including salary), sometimes
+    /// as a result of a court order. Wage garnishments continue until the entire debt is paid or arrangements are made to pay off the debt. Garnishments can be taken
+    /// for any type of debt but common examples of debt that result in garnishments include:
+    /// child support, defaulted student loans, taxes, unpaid court fines
+    /// When served on an employer, garnishments are taken as part of the payroll process. When processing payroll, sometimes there is not enough money in the employee's
+    /// net pay to satisfy all of the garnishments. For example, in a case with federal tax, local tax, and credit card garnishments, the first garnishment taken would be the
+    /// federal tax garnishments, then the local tax garnishments, and finally, garnishments for the credit card. Employers receive a notice telling them to withhold a certain
+    /// amount of their employee's wages for payment and cannot refuse to garnish wages. Employers must correctly calculate the amount to withhold, and must make the deductions until the garnishment expires.
+    /// (Source: Wikipedia)
     /// </summary>
+    /// <seealso cref="http://en.wikipedia.org/wiki/Garnishment"/>
     public enum GarnishmentType
     {
+        // TODO -- These "SHOULD" be in order of application, as described above
+
+        [Description("Unpaid Federal Taxes")]
+        UnpaidFederalTaxes = 1,
+
+        [Description("Unpaid State Taxes")]
+        UnpaidStateTaxes = 2,
+
+        [Description("Unpaid Local Taxes")]
+        UnpaidLocalTaxes = 3,
+
         [Description("Child Support")]
-        ChildSupport = 1,
+        ChildSupport = 10,
 
-        [Description("Unpaid Taxes")]
-        UnpaidTaxes = 2,
+        Bankruptcy = 15,
 
-        Creditor = 3,
-
-        Bankruptcy = 4,
+        Creditor = 16,
 
         [Description("Student Loans")]
-        StudentLoans = 5
+        StudentLoans = 17
     }
 
     public class GarnishmentTypes
     {
         public static IEnumerable<GarnishmentType> Values()
         {
+            yield return GarnishmentType.UnpaidFederalTaxes;
+            yield return GarnishmentType.UnpaidStateTaxes;
+            yield return GarnishmentType.UnpaidLocalTaxes;
+
             yield return GarnishmentType.ChildSupport;
-            yield return GarnishmentType.UnpaidTaxes;
+
             yield return GarnishmentType.Creditor;
             yield return GarnishmentType.Bankruptcy;
             yield return GarnishmentType.StudentLoans;
