991faea4b998b349ae37bfad85401bd2b6d41f62
Matt Wagner <mattgwagner@gmail.com>
2015-02-18 11:32:05 -0500

    Update to a common project layout and script setup

--- Build-Package.ps1
+++
@@ -1,5 +0,0 @@
-$BaseDir = Split-Path (Resolve-Path $MyInvocation.MyCommand.Path)
-
-mkdir "$BaseDir\Artifacts" -Force
-
-& "$BaseDir\.NuGet\NuGet.exe" Pack CertiPay.Payroll.Common\CertiPay.Payroll.Common.nuspec -Properties Configuration=Release -OutputDirectory "$BaseDir\artifacts\"
