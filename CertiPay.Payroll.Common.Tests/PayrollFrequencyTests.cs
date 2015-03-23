0438aa69f336b47ddd5121b1ee684dc6cdd5fcd2
Matt Wagner <mattgwagner@gmail.com>
2015-03-23 11:07:21 -0400

    Use range and theory to test a larger range

--- CertiPay.Payroll.Common.Tests/PayrollFrequencyTests.cs
+++ CertiPay.Payroll.Common.Tests/PayrollFrequencyTests.cs
@@ -16,28 +16,16 @@ namespace CertiPay.Payroll.Common.Tests
             Assert.AreEqual(expectedPeriods, freq.AnnualizedPayPeriods());
         }
 
-        [Test]
-        [TestCase(PayrollFrequency.Monthly, 1000, 12000)]
-        [TestCase(PayrollFrequency.BiWeekly, 1000, 26000)]
-        [TestCase(PayrollFrequency.Weekly, 1000, 52000)]
-        [TestCase(PayrollFrequency.SemiMonthly, 1000, 24000)]
-        [TestCase(PayrollFrequency.Daily, 1000, 260000)]
-        [TestCase(PayrollFrequency.BiWeekly, 1234.56, 32098.56)]
-        public void Should_Calculate_Annualized_Pay(PayrollFrequency freq, Decimal payPeriod, Decimal expected)
+        [Theory]
+        public void Should_Calculate_Annualized_Pay(PayrollFrequency freq, [Random(0, 10000.00, 50)]Decimal payPeriod)
         {
-            Assert.AreEqual(expected, freq.CalculateAnnualized(payPeriod));
+            Assert.That(freq.CalculateAnnualized(payPeriod), Is.EqualTo(freq.AnnualizedPayPeriods() * payPeriod));
         }
 
-        [Test]
-        [TestCase(PayrollFrequency.Monthly, 1000, 12000)]
-        [TestCase(PayrollFrequency.BiWeekly, 1000, 26000)]
-        [TestCase(PayrollFrequency.Weekly, 1000, 52000)]
-        [TestCase(PayrollFrequency.SemiMonthly, 1000, 24000)]
-        [TestCase(PayrollFrequency.Daily, 1000, 260000)]
-        [TestCase(PayrollFrequency.BiWeekly, 1234.56, 32098.56)]
-        public void Should_Calculate_DeAnnualized_Pay(PayrollFrequency freq, Decimal expected, Decimal perYear)
+        [Theory]
+        public void Should_Calculate_DeAnnualized_Pay(PayrollFrequency freq, [Random(0, 300000.00, 50)]Decimal perYear)
         {
-            Assert.AreEqual(expected, freq.CalculateDeannualized(perYear));
+            Assert.That(freq.CalculateDeannualized(perYear), Is.EqualTo(perYear / freq.AnnualizedPayPeriods()).Within(0.01));
         }
     }
 }
