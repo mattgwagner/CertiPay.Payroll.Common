c959160328a2ac56e03393bf8841fea106f7deb1
Matt Wagner <mattgwagner@gmail.com>
2015-07-27 21:25:05 -0400

    Added bank account model and routing number validation

---
+++ CertiPay.Payroll.Common.Tests/BankAccountTests.cs
@@ -0,0 +1,23 @@
+﻿using NUnit.Framework;
+using System;
+
+namespace CertiPay.Payroll.Common.Tests
+{
+    public class BankAccountTests
+    {
+        [Theory]
+        [TestCase("111000025", true)] // BoA in Virginia
+        [TestCase("021000021", true)] // JPMorgan
+        [TestCase("053100465", true)] // SunTrust
+        [TestCase("263177563", true)] // MIDFLORIDA
+        
+        [TestCase("100000007", false)]
+        [TestCase("111111118", false)]
+        [TestCase("100000007", false)]
+        [TestCase("100000007", false)]
+        public void Validate_Routing_Number(String routingNumber, Boolean is_valid)
+        {
+            Assert.AreEqual(is_valid, BankAccount.IsValidRoutingNumber(routingNumber), "Routing # validation failed");
+        }
+    }
+}
---
+++ CertiPay.Payroll.Common/BankAccount.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace CertiPay.Payroll.Common
+{
+    [ComplexType]
+    public class BankAccount
+    {
+        // TODO Code to check for validity of MICR number?
+
+        /// <summary>
+        /// The 9-digit routing number that identifies the financial institution that manages the account
+        /// </summary>
+        [Display(Name = "Routing Number", ShortName = "MICR #")]
+        [StringLength(9)]
+        public String RoutingNumber { get; set; }
+
+        /// <summary>
+        /// Up to 17-digit number that identifies a specific account at a financial institution.
+        /// NACHA allows up to 17 in their feed spec, but most are ~8-10
+        /// </summary>
+        [Display(Name = "Account Number", ShortName = "Acct #")]
+        [StringLength(17)]
+        public String AccountNumber { get; set; }
+
+        /// <summary>
+        /// True if the bank account is a savings account rather than a checking account
+        /// </summary>
+        [Display(Name = "Is Savings?")]
+        public Boolean IsSavings { get; set; }
+
+        /// <summary>
+        /// Returns true if the number provided passes the checksum validation for routing numbers
+        /// </summary>
+        public static Boolean IsValidRoutingNumber(String routingNumber)
+        {
+            //The MICR number is of the form: XXXXYYYYC where XXXX is Federal Reserve Routing Symbol, YYYY is ABA Institution Identifier, and C is the Check Digit
+
+            if (!String.IsNullOrWhiteSpace(routingNumber) && routingNumber.Length == 9)
+            {
+                String fedReserveRoutingSymbol = routingNumber.Substring(0, 4);
+
+                //The first two digits of the nine digit RTN must be in the ranges 00 through 12, 21 through 32, 61 through 72, or 80.
+
+                //The digits are assigned as follows:
+
+                //00 is used by the United States Government
+                //01 through 12 are the "normal" routing numbers, and correspond to the 12 Federal Reserve Banks.For example, 0260 - 0959 - 3 is the routing number for Bank of America incoming wires in New York, with the initial "02" indicating the Federal Reserve Bank of New York.
+                //21 through 32 were assigned only to thrift institutions(e.g.credit unions and savings banks) through 1985, but are no longer assigned(thrifts are assigned normal 01–12 numbers).Currently they are still used by the thrift institutions, or their successors, and correspond to the normal routing number, plus 20. (For example, 2260 - 7352 - 3 is the routing number for Grand Adirondack Federal Credit Union in New York, with the initial "22" corresponding to "02"(New York Fed) plus "20"(thrift).)
+                //61 through 72 are special purpose routing numbers designated for use by non - bank payment processors and clearinghouses and are termed Electronic Transaction Identifiers(ETIs), and correspond to the normal routing number, plus 60.
+                //80 is used for traveler's cheques
+
+                String abaIdentifier = routingNumber.Substring(4, 4);
+
+                String checkDigit = routingNumber.Substring(8, 1);
+
+                return checkDigit == GetCheckDigit(routingNumber);
+            }
+
+            return false;
+        }
+
+        private static String GetCheckDigit(String routingNumber)
+        {
+            //The ninth, check digit provides a checksum test using a position-weighted sum of each of the digits.
+            // High - speed check - sorting equipment will typically verify the checksum and if it fails, route the item
+            // to a reject pocket for manual examination, repair, and re - sorting.Mis - routings to an incorrect bank are thus greatly reduced.
+
+            // Checksum = 3x(d[1] + d[4] + d[7]) + 7x(d[2] + d[5] + d[8]) + (d[3] + d[6] + d[9]) % 10 == 0
+
+            return (
+                (3 * (Convert.ToInt32(routingNumber[0]) + Convert.ToInt32(routingNumber[3]) + Convert.ToInt32(routingNumber[6])))
+                + (7 * (Convert.ToInt32(routingNumber[6]) + Convert.ToInt32(routingNumber[4]) + Convert.ToInt32(routingNumber[7])))
+                + ((Convert.ToInt32(routingNumber[2]) + Convert.ToInt32(routingNumber[5]) + Convert.ToInt32(routingNumber[8]))))
+                .ToString();
+        }
+    }
+}
