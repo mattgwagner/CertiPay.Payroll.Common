fee54f95d79086e03bdf3b8fafc8ef3d8effd9e8
Matt Wagner <mattgwagner@gmail.com>
2015-08-12 13:43:54 -0400

    Add exempt/nonexempt status to list of payroll enums

---
+++ CertiPay.Payroll.Common/FLSAStatus.cs
@@ -0,0 +1,21 @@
+﻿namespace CertiPay.Payroll.Common
+{
+    /// <summary>
+    /// The Fair Labor Standards Act uses the terms exempt and nonexempt to describe work that is included (nonexempt)
+    /// or not included (exempt) in the Act’s overtime and record-keeping provisions. The Act requires that overtime (payment
+    /// for hours worked in excess of 40 hours in one week) be paid to employees performing nonexempt work. The Act exempts,
+    ///  or does not require, that time and one half be paid to employees performing exempt work.
+    /// </summary>
+    public enum FLSAStatus : byte
+    {
+        /// <summary>
+        /// Employee is allowed to earn overtime for performing the work in excess of 40 hours in one week
+        /// </summary>
+        Nonexempt,
+
+        /// <summary>
+        /// Employee is not allowed or exempt from earning overtime for performing work in excess of 40 hours in one week
+        /// </summary>
+        Exempt
+    }
+}
