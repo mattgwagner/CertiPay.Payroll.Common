d5758e59d11e4e8cc27f174d871d6ca76f028b46
Matt Wagner <mattgwagner@gmail.com>
2015-11-05 09:47:57 -0500

    Add enum for calendar quarters

---
+++ CertiPay.Payroll.Common.Tests/QuarterOfYearTests.cs
@@ -0,0 +1,44 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CertiPay.Payroll.Common.Tests
+{
+    public class QuarterOfYearTests
+    {
+        const int year = 2015;
+
+        [Test]
+        [TestCase(QuarterOfYear.Q1, "1/1/2015")]
+        [TestCase(QuarterOfYear.Q2, "4/1/2015")]
+        [TestCase(QuarterOfYear.Q3, "7/1/2015")]
+        [TestCase(QuarterOfYear.Q4, "10/1/2015")]
+        public void Returns_Start_Of_Quarter(QuarterOfYear quarter, String expected)
+        {
+            Assert.AreEqual(DateTime.Parse(expected), quarter.Start(year));
+        }
+
+        [Test]
+        [TestCase(QuarterOfYear.Q1, "3/31/2015")]
+        [TestCase(QuarterOfYear.Q2, "6/30/2015")]
+        [TestCase(QuarterOfYear.Q3, "9/30/2015")]
+        [TestCase(QuarterOfYear.Q4, "12/31/2015")]
+        public void Returns_End_of_Quarter(QuarterOfYear quarter, String expected)
+        {
+            Assert.AreEqual(DateTime.Parse(expected), quarter.End(year));
+        }
+
+        [Test]
+        [TestCase("1/10/2015", QuarterOfYear.Q1)]
+        [TestCase("10/1/2015", QuarterOfYear.Q4)]
+        [TestCase("3/15/2015", QuarterOfYear.Q1)]
+        [TestCase("9/30/2015", QuarterOfYear.Q3s)]
+        public void Returns_Expected_Quarter(String date, QuarterOfYear expected)
+        {
+            Assert.AreEqual(expected, DateTime.Parse(date).GetQuarter());
+        }
+    }
+}
---
+++ CertiPay.Payroll.Common/QuarterOfYear.cs
@@ -0,0 +1,102 @@
+﻿using System;
+
+namespace CertiPay.Payroll.Common
+{
+    /// <summary>
+    /// Represents the year broken into four quarters, commonly for tax or reporting purposes
+    /// </summary>
+    public enum QuarterOfYear : byte
+    {
+        /// <summary>
+        /// January, February, March
+        /// </summary>
+        Q1 = 1,
+
+        /// <summary>
+        /// April, May, June
+        /// </summary>
+        Q2 = 2,
+
+        /// <summary>
+        /// July, August, September
+        /// </summary>
+        Q3 = 3,
+
+        /// <summary>
+        /// October, November, December
+        /// </summary>
+        Q4 = 4
+    }
+
+    public static class QuarterOfYearExtensions
+    {
+        /// <summary>
+        /// Returns the first day of the quarter.
+        ///
+        /// If no year is provided, it will return for the current year
+        /// </summary>
+        public static DateTime Start(this QuarterOfYear quarter, int? year = null)
+        {
+            switch (quarter)
+            {
+                case QuarterOfYear.Q1:
+                    return new DateTime(year ?? DateTime.Today.Year, 1, 1);
+
+                case QuarterOfYear.Q2:
+                    return new DateTime(year ?? DateTime.Today.Year, 4, 1);
+
+                case QuarterOfYear.Q3:
+                    return new DateTime(year ?? DateTime.Today.Year, 7, 1);
+
+                case QuarterOfYear.Q4:
+                    return new DateTime(year ?? DateTime.Today.Year, 10, 1);
+
+                default:
+                    throw new ArgumentOutOfRangeException("There's only four quarters?");
+            }
+        }
+
+        /// <summary>
+        /// Returns the last day of the quarter.
+        ///
+        /// If no year is provided, it will return for the current year
+        /// </summary>
+        public static DateTime End(this QuarterOfYear quarter, int? year = 0)
+        {
+            switch (quarter)
+            {
+                case QuarterOfYear.Q1:
+                    return new DateTime(year ?? DateTime.Today.Year, 3, 31);
+
+                case QuarterOfYear.Q2:
+                    return new DateTime(year ?? DateTime.Today.Year, 6, 30);
+
+                case QuarterOfYear.Q3:
+                    return new DateTime(year ?? DateTime.Today.Year, 9, 30);
+
+                case QuarterOfYear.Q4:
+                    return new DateTime(year ?? DateTime.Today.Year, 12, 31);
+
+                default:
+                    throw new ArgumentOutOfRangeException("There's only four quarters?");
+            }
+        }
+
+        /// <summary>
+        /// Returns the QuarterOfYear that the date falls into
+        /// </summary>
+        public static QuarterOfYear GetQuarter(this DateTime date)
+        {
+            if (date < QuarterOfYear.Q2.Start(date.Year))
+                return QuarterOfYear.Q1;
+
+            if (date < QuarterOfYear.Q3.Start(date.Year))
+                return QuarterOfYear.Q2;
+
+            if (date < QuarterOfYear.Q4.Start(date.Year))
+                return QuarterOfYear.Q3;
+
+            return QuarterOfYear.Q4;
+        }
+    }
+}
