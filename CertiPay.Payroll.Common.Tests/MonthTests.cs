09007c8cc4efa3a7ba9a67760dae87319d822ae3
Matt Wagner <mattgwagner@gmail.com>
2015-11-24 09:37:38 -0500

    Add enum for month, handling first/last day of the month

---
+++ CertiPay.Payroll.Common.Tests/MonthTests.cs
@@ -0,0 +1,45 @@
+﻿using NUnit.Framework;
+using System;
+
+namespace CertiPay.Payroll.Common.Tests
+{
+    public class MonthTests
+    {
+        private const int year = 2015;
+
+        [Test]
+        [TestCase("JANUARY", Month.January)]
+        [TestCase("february", Month.February)]
+        [TestCase("maRcH", Month.March)]
+        [TestCase("April", Month.April)]
+        [TestCase("May", Month.May)]
+        [TestCase("June", Month.June)]
+        [TestCase("July", Month.July)]
+        [TestCase("August", Month.August)]
+        [TestCase("September", Month.September)]
+        [TestCase("October", Month.October)]
+        [TestCase("November", Month.November)]
+        [TestCase("December", Month.December)]
+        public void Parse(String month, Month expected)
+        {
+            Assert.AreEqual(expected, Months.Parse(month));
+        }
+
+        [Test]
+        [TestCase(Month.January, "1/1/2015")]
+        [TestCase(Month.December, "12/1/2015")]
+        public void FirstDayOfMonth(Month month, String expected)
+        {
+            Assert.AreEqual(DateTime.Parse(expected), month.FirstDayOfMonth(year));
+        }
+
+        [Test]
+        [TestCase(Month.January, "1/31/2015")]
+        [TestCase(Month.June, "6/30/2015")]
+        [TestCase(Month.December, "12/31/2015")]
+        public void LastDayOfMonth(Month month, String expected)
+        {
+            Assert.AreEqual(DateTime.Parse(expected), month.LastDayOfMonth(year));
+        }
+    }
+}
---
+++ CertiPay.Payroll.Common/Month.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace CertiPay.Payroll.Common
+{
+    /// <summary>
+    /// Represents the months of the calendar year.
+    /// </summary>
+    public enum Month : byte
+    {
+        January = 1,
+
+        February = 2,
+
+        March = 3,
+
+        April = 4,
+
+        May = 5,
+
+        June = 6,
+
+        July = 7,
+
+        August = 8,
+
+        September = 9,
+
+        October = 10,
+
+        November = 11,
+
+        December = 12
+    }
+
+    public static class Months
+    {
+        /// <summary>
+        /// Returns the months of the year
+        /// </summary>
+        public static IEnumerable<Month> Values()
+        {
+            foreach (Month month in Enum.GetValues(typeof(Month)))
+            {
+                yield return month;
+            }
+        }
+
+        /// <summary>
+        /// Parses the case-insensitive string representation of the month (i.e. January, March...)
+        /// </summary>
+        public static Month Parse(String value)
+        {
+            return (Month)Enum.Parse(typeof(Month), value, ignoreCase: true);
+        }
+
+        /// <summary>
+        /// Returns the date time of the first day of the given month and optional year.
+        ///
+        /// If no year is provided, the current calendar year is used.
+        /// </summary>
+        public static DateTime FirstDayOfMonth(this Month month, int? year = null)
+        {
+            return new DateTime(year ?? DateTime.Today.Year, (int)month, 1);
+        }
+
+        /// <summary>
+        /// Returns the date time of the last day of the given month and optional year.
+        ///
+        /// If no year is provided, the current calendar year is used.
+        /// </summary>
+        public static DateTime LastDayOfMonth(this Month month, int? year = null)
+        {
+            return
+                FirstDayOfMonth(month, year)
+                .AddMonths(1)
+                .AddDays(-1);
+        }
+    }
+}
