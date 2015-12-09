6e574134a99d623b436b128f5d87d76c5a1b7aeb
Matt Wagner <mattgwagner@gmail.com>
2015-12-09 11:00:10 -0500

    Add the Address model for common usage

---
+++ CertiPay.Payroll.Common/Address.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace CertiPay.Payroll.Common
+{
+    [ComplexType]
+    public class Address
+    {
+        // Warning: This is marked as a complex type, and used in several projects, so any changes will trickle down into EF changes...
+
+        [StringLength(75)]
+        public String Address1 { get; set; }
+
+        [StringLength(75)]
+        public String Address2 { get; set; }
+
+        [StringLength(75)]
+        public String Address3 { get; set; }
+
+        [StringLength(50)]
+        public String City { get; set; }
+
+        public StateOrProvince State { get; set; }
+
+        [DataType(DataType.PostalCode)]
+        [StringLength(15)]
+        [Display(Name = "Postal Code")]
+        public String PostalCode { get; set; }
+
+        // TODO: international fields
+
+        public Address()
+        {
+            this.State = StateOrProvince.FL;
+        }
+    }
+}
