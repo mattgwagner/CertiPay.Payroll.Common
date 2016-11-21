5370be3472f3024c19420d877f96828bcb12aefb
Matt Wagner <mattgwagner@gmail.com>
2016-11-21 14:19:27 -0500

    Update build script, finish porting test project

--- .build/Build.ps1
+++ .build/Build.ps1
@@ -1,7 +1,4 @@
 param (
-	## The build target, i.e. Build, Rebuild, Clean
-	[string]$Target = 'Build',
-
 	## The build configuration, i.e. Debug/Release
 	[string]$Configuration = 'Debug'
 )
@@ -12,18 +9,8 @@ $Here = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
 
 $SolutionRoot = (Split-Path -parent $Here)
 
-$SolutionFile = Join-Path $SolutionRoot "CertiPay.Payroll.Common"
-
-# Bootstap ensures we have what we need to build the project
-
 $DotNet = "${env:ProgramFiles}\dotnet\dotnet.exe"
 
-# Build the solution, packaging the files if requested
-
-# dotnet.exe build "C:\Users\Administrator\Documents\GitHub\certipay.payroll.common\CertiPay.Payroll.Common" --configuration Debug --no-dependencies --no-incremental
-
-Write-Output "Running build target $Target for $Configuration"
-
-& $DotNet build $SolutionFile --configuration $Configuration
+& $DotNet build */**/project.json --configuration $Configuration
 
 EXIT $LASTEXITCODE
